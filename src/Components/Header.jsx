ec08ff5039c5a77094c36951b2cbdd798cb6ff41
Ysmael Braga <ysmaelbraga01@gmail.com>
2025-04-24 16:46:00 -0300

    ajustes no layout do header e inserido arquivos svg no projeto

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
+import Dog from "../Assets/dogs.svg?react";
+
 const Header = () => {
   return (
     <div className={styles.header}>
-      <nav className="container">
-        <Link to="/">Home</Link>
-        <Link to="/login">Login / Criar</Link>
+      <nav className={`${styles.nav} container`}>
+        <Link to="/" className={styles.logo} aria-label="Dogs - Home">
+          <Dog />
+        </Link>
+        <Link to="/login" className={styles.login}>
+          Login / Criar
+        </Link>
       </nav>
     </div>
   );
