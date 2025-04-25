e4a4860287d47585c999134f785239533e91d9e3
Ysmael Braga <ysmaelbraga01@gmail.com>
2025-04-24 23:00:57 -0300

    criando e utilizando os hooks

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
 
 const LoginForm = () => {
-  const [username, setUsername] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const username = useForm("email");
+  const password = useForm();
+
   function handleSubmit(event) {
     event.preventDefault();
     fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
@@ -13,7 +15,7 @@ const LoginForm = () => {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ username, password }),
+      body: JSON.stringify(),
     })
       .then((response) => {
         console.log(response);
@@ -27,8 +29,8 @@ const LoginForm = () => {
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="username" />
-        <Input label="Senha" type="password" name="password" />
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
         <Button disabled>Entrar</Button>
       </form>
       <Link to="/login/criar">Cadastro</Link>
