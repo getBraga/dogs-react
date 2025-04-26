4c68efeba7bb22d9e7b557769dca01249c06ead9
Ysmael Braga <ysmaelbraga01@gmail.com>
2025-04-26 00:37:32 -0300

    inserindo mais propriedades no componente

--- src/Components/Forms/Input.jsx
+++ src/Components/Forms/Input.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import styles from "./Input.module.css";
-const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
+const Input = ({
+  label,
+  type,
+  name,
+  value,
+  onChange,
+  error,
+  onBlur,
+  autoComplete,
+}) => {
   return (
     <div className={styles.wrapper}>
       <label htmlFor={name} className={styles.label}>
@@ -14,6 +23,7 @@ const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
         onChange={onChange}
         value={value}
         onBlur={onBlur}
+        autoComplete={autoComplete}
       />
 
       {error && <p className={styles.error}>{error}</p>}
