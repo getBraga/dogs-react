beabf222177dddb94f0e7ef23513f05698cbddf2
Ysmael Braga <ysmaelbraga01@gmail.com>
2025-04-28 16:12:38 -0300

    usando e configurando a protecao de rotas

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { UserStorage } from "./UserContext";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
+import User from "./Components/User/User";
+import ProtectedRoute from "./Helper/ProtectedRoute";
 const App = () => {
   return (
     <div>
@@ -12,8 +14,23 @@ const App = () => {
         <UserStorage>
           <Header />
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="login/*" element={<Login />} />
+            <Route
+              path="conta/*"
+              element={
+                <ProtectedRoute>
+                  <Login />
+                </ProtectedRoute>
+              }
+            />
           </Routes>
           <Footer />
         </UserStorage>
