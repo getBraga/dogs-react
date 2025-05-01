6c2c56b927008d72c2dfd5a547c252ff8bedd534
Ysmael Braga <ysmaelbraga01@gmail.com>
2025-05-01 14:06:06 -0300

    como nao esta sendo utilizado nenhum react.effect decidi por  usar apenas uma funcao assincrona sem o react.userCallback

--- src/Hooks/useFetch.jsx
+++ src/Hooks/useFetch.jsx
@@ -4,7 +4,7 @@ export const useFetch = () => {
   const [data, setData] = React.useState(null);
   const [error, setError] = React.useState(null);
   const [loading, setLoading] = React.useState(false);
-  const request = React.useCallback(async (url, options) => {
+  const request = async (url, options) => {
     let response;
     let json;
     try {
@@ -21,7 +21,7 @@ export const useFetch = () => {
       setLoading(false);
       return { response, json };
     }
-  }, []);
+  };
   return {
     data,
     loading,
